954dfe584b74f813cb57705a6ed322bf898785b6
MoreTorque <104254025+MoreTore@users.noreply.github.com>
2024-12-06 17:28:25 -0600

    Update athenad.py

--- system/athena/athenad.py
+++ system/athena/athenad.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import base64
+import bz2
 import hashlib
 import io
 import json
@@ -14,7 +15,6 @@ import sys
 import tempfile
 import threading
 import time
-import zstandard as zstd
 import logging
 import asyncio
 from dataclasses import asdict, dataclass, replace
@@ -37,7 +37,6 @@ from openpilot.common.file_helpers import CallbackReader
 from openpilot.common.params import Params
 from openpilot.common.realtime import set_core_affinity
 from openpilot.system.hardware import HARDWARE, PC
-from openpilot.system.loggerd.uploader import LOG_COMPRESSION_LEVEL
 from openpilot.system.loggerd.xattr_cache import getxattr, setxattr
 from openpilot.common.swaglog import cloudlog
 from openpilot.system.version import get_build_metadata
@@ -45,9 +44,9 @@ from openpilot.system.hardware.hw import Paths
 from openpilot.system.athena.webrtc_helpers import webrtc_event_loop
 
 
-ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.comma.ai')
+ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.springerelectronics.com')
 HANDLER_THREADS = int(os.getenv('HANDLER_THREADS', "4"))
-LOCAL_PORT_WHITELIST = {22, }  # SSH
+LOCAL_PORT_WHITELIST = {8022}
 
 LOG_ATTR_NAME = 'user.upload'
 LOG_ATTR_VALUE_MAX_UNIX_TIME = int.to_bytes(2147483647, 4, sys.byteorder)
@@ -103,13 +102,16 @@ upload_queue: Queue[UploadItem] = queue.Queue()
 low_priority_send_queue: Queue[str] = queue.Queue()
 log_recv_queue: Queue[str] = queue.Queue()
 sdp_recv_queue = queue.Queue()
+sdp_send_queue = queue.Queue()
+ice_send_queue = queue.Queue()
 cancelled_uploads: set[str] = set()
 
 cur_upload_items: dict[int, UploadItem | None] = {}
+sdp_cache = None
+ice_cache = None
 
-
-def strip_zst_extension(fn: str) -> str:
-  if fn.endswith('.zst'):
+def strip_bz2_extension(fn: str) -> str:
+  if fn.endswith('.bz2'):
     return fn[:-4]
   return fn
 
@@ -150,7 +152,7 @@ def handle_long_poll(ws: WebSocket, exit_event: threading.Event | None) -> None:
     threading.Thread(target=upload_handler, args=(end_event,), name='upload_handler'),
     threading.Thread(target=log_handler, args=(end_event,), name='log_handler'),
     threading.Thread(target=stat_handler, args=(end_event,), name='stat_handler'),
-    threading.Thread(target=rtc_handler, args=(end_event, send_queue, sdp_recv_queue), name='rtc_handler'),
+    threading.Thread(target=rtc_handler, args=(end_event, sdp_send_queue, sdp_recv_queue, ice_send_queue), name='rtc_handler'),
   ] + [
     threading.Thread(target=jsonrpc_handler, args=(end_event,), name=f'worker_{x}')
     for x in range(HANDLER_THREADS)
@@ -170,6 +172,47 @@ def handle_long_poll(ws: WebSocket, exit_event: threading.Event | None) -> None:
       cloudlog.debug(f"athena.joining {thread.name}")
       thread.join()
 
+@dispatcher.add_method
+def rtc_handler(end_event: threading.Event, send_queue: queue.Queue, sdp_recv_queue: queue.Queue, ice_recv_queue: queue.Queue) -> None:
+  logging.basicConfig(level=logging.INFO)
+  loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+
+  try:
+    loop.run_until_complete(webrtc_event_loop(end_event, send_queue, sdp_recv_queue, ice_recv_queue))
+  finally:
+    print("Closing event loop")
+    loop.close()
+    print("Event loop closed. Thread joining.")
+
+@dispatcher.add_method
+def setSdpAnswer(answer):
+  print(f"[Athenad DEBUG] setSdpAnswer({answer=})")
+  sdp_recv_queue.put_nowait(answer)
+
+@dispatcher.add_method
+def getSdp():
+  global sdp_cache
+  if not sdp_send_queue.empty():
+    sdp_cache = json.loads(sdp_send_queue.get_nowait())
+
+  if sdp_cache:
+    print(f"[Athenad DEBUG] getSdp({sdp_cache=})")
+    return sdp_cache
+  else:
+    return {
+      'error': True
+    }
+
+@dispatcher.add_method
+def getIce():
+  print("[Athenad DEBUG] getIce()")
+  if not ice_send_queue.empty():
+    return json.loads(ice_send_queue.get_nowait())
+  else:
+    return {
+      'error': True
+    }
 
 def jsonrpc_handler(end_event: threading.Event) -> None:
   dispatcher["startLocalProxy"] = partial(startLocalProxy, end_event)
@@ -190,21 +233,6 @@ def jsonrpc_handler(end_event: threading.Event) -> None:
       cloudlog.exception("athena jsonrpc handler failed")
       send_queue.put_nowait(json.dumps({"error": str(e)}))
 
-def rtc_handler(end_event: threading.Event, send_queue: queue.Queue, sdp_recv_queue: queue.Queue) -> None:
-  logging.basicConfig(level=logging.DEBUG)
-  loop = asyncio.new_event_loop()
-  asyncio.set_event_loop(loop)
-
-  try:
-    loop.run_until_complete(webrtc_event_loop(end_event, send_queue, sdp_recv_queue))
-  finally:
-    print("Closing event loop")
-    loop.close()
-    print("Event loop closed. Thread joining.")
-
-@dispatcher.add_method
-def putSdpMessage(sdp):
-  sdp_recv_queue.put_nowait(sdp)
 
 def retry_upload(tid: int, end_event: threading.Event, increase_count: bool = True) -> None:
   item = cur_upload_items[tid]
@@ -278,13 +306,13 @@ def upload_handler(end_event: threading.Event) -> None:
           sz = -1
 
         cloudlog.event("athena.upload_handler.upload_start", fn=fn, sz=sz, network_type=network_type, metered=metered, retry_count=item.retry_count)
+        response = _do_upload(item, partial(cb, sm, item, tid, end_event))
 
-        with _do_upload(item, partial(cb, sm, item, tid, end_event)) as response:
-          if response.status_code not in (200, 201, 401, 403, 412):
-            cloudlog.event("athena.upload_handler.retry", status_code=response.status_code, fn=fn, sz=sz, network_type=network_type, metered=metered)
-            retry_upload(tid, end_event)
-          else:
-            cloudlog.event("athena.upload_handler.success", fn=fn, sz=sz, network_type=network_type, metered=metered)
+        if response.status_code not in (200, 201, 401, 403, 412):
+          cloudlog.event("athena.upload_handler.retry", status_code=response.status_code, fn=fn, sz=sz, network_type=network_type, metered=metered)
+          retry_upload(tid, end_event)
+        else:
+          cloudlog.event("athena.upload_handler.success", fn=fn, sz=sz, network_type=network_type, metered=metered)
 
         UploadQueueCache.cache(upload_queue)
       except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.SSLError):
@@ -304,16 +332,16 @@ def _do_upload(upload_item: UploadItem, callback: Callable = None) -> requests.R
   path = upload_item.path
   compress = False
 
-  # If file does not exist, but does exist without the .zst extension we will compress on the fly
-  if not os.path.exists(path) and os.path.exists(strip_zst_extension(path)):
-    path = strip_zst_extension(path)
+  # If file does not exist, but does exist without the .bz2 extension we will compress on the fly
+  if not os.path.exists(path) and os.path.exists(strip_bz2_extension(path)):
+    path = strip_bz2_extension(path)
     compress = True
 
   with open(path, "rb") as f:
     content = f.read()
     if compress:
       cloudlog.event("athena.upload_handler.compress", fn=path, fn_orig=upload_item.path)
-      content = zstd.compress(content, LOG_COMPRESSION_LEVEL)
+      content = bz2.compress(content)
 
   with io.BytesIO(content) as data:
     return requests.put(upload_item.url,
@@ -329,16 +357,13 @@ def getMessage(service: str, timeout: int = 1000) -> dict:
     raise Exception("invalid service")
 
   socket = messaging.sub_sock(service, timeout=timeout)
-  try:
-    ret = messaging.recv_one(socket)
+  ret = messaging.recv_one(socket)
 
-    if ret is None:
-      raise TimeoutError
+  if ret is None:
+    raise TimeoutError
 
-    # this is because capnp._DynamicStructReader doesn't have typing information
-    return cast(dict, ret.to_dict())
-  finally:
-    del socket
+  # this is because capnp._DynamicStructReader doesn't have typing information
+  return cast(dict, ret.to_dict())
 
 
 @dispatcher.add_method
@@ -350,6 +375,19 @@ def getVersion() -> dict[str, str]:
     "branch": build_metadata.channel,
     "commit": build_metadata.openpilot.git_commit,
   }
+
+
+@dispatcher.add_method
+def setNavDestination(latitude: int = 0, longitude: int = 0, place_name: str = None, place_details: str = None) -> dict[str, int]:
+  destination = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "place_name": place_name,
+    "place_details": place_details,
+  }
+  Params().put("NavDestination", json.dumps(destination))
+
+  return {"success": 1}
 
 
 def scan_dir(path: str, prefix: str) -> list[str]:
@@ -399,7 +437,7 @@ def uploadFilesToUrls(files_data: list[UploadFileDict]) -> UploadFilesToUrlRespo
       continue
 
     path = os.path.join(Paths.log_root(), file.fn)
-    if not os.path.exists(path) and not os.path.exists(strip_zst_extension(path)):
+    if not os.path.exists(path) and not os.path.exists(strip_bz2_extension(path)):
       failed.append(file.fn)
       continue
 
@@ -425,7 +463,6 @@ def uploadFilesToUrls(files_data: list[UploadFileDict]) -> UploadFilesToUrlRespo
 
   resp: UploadFilesToUrlResponse = {"enqueued": len(items), "items": items}
   if failed:
-    cloudlog.event("athena.uploadFilesToUrls.failed", failed=failed, error=True)
     resp["failed"] = failed
 
   return resp
@@ -468,10 +505,6 @@ def setRouteViewed(route: str) -> dict[str, int | str]:
 
 def startLocalProxy(global_end_event: threading.Event, remote_ws_uri: str, local_port: int) -> dict[str, int]:
   try:
-    # migration, can be removed once 0.9.8 is out for a while
-    if local_port == 8022:
-      local_port = 22
-
     if local_port not in LOCAL_PORT_WHITELIST:
       raise Exception("Requested local port not whitelisted")
 
@@ -545,6 +578,17 @@ def getNetworkMetered() -> bool:
 @dispatcher.add_method
 def getNetworks():
   return HARDWARE.get_networks()
+
+
+@dispatcher.add_method
+def getManagerURL():
+  ip_addr = HARDWARE.get_ipv4_address()
+  if ip_addr:
+    return {
+      'url': f"http://{ip_addr}:8082",
+    }
+  else:
+    raise Exception("Network Error")
 
 
 @dispatcher.add_method
@@ -649,9 +693,8 @@ def log_handler(end_event: threading.Event) -> None:
 
 def stat_handler(end_event: threading.Event) -> None:
   STATS_DIR = Paths.stats_root()
-  last_scan = 0.0
-
   while not end_event.is_set():
+    last_scan = 0.
     curr_scan = time.monotonic()
     try:
       if curr_scan - last_scan > 10:
@@ -825,8 +868,6 @@ def main(exit_event: threading.Event = None):
       cur_upload_items.clear()
 
       handle_long_poll(ws, exit_event)
-
-      ws.close()
     except (KeyboardInterrupt, SystemExit):
       break
     except (ConnectionError, TimeoutError, WebSocketException):
