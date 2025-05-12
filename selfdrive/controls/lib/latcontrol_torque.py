dc2c7e37ba13566d96506bef50ef0fa60f5bb55a
MoreTore <ryleymccarroll22@gmail.com>
2025-05-12 12:21:10 -0500

    wheel inertia

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -87,7 +87,7 @@ class LatControlTorque(LatControl):
       raw_alpha = (rate_rad - self.prev_rate_rad) / DT_CTRL
       self.prev_rate_rad = rate_rad
       filt_alpha = self.inertia_filter.update(raw_alpha)
-      inertia_ff = self.I_sw * filt_alpha
+      inertia_ff = -self.I_sw * filt_alpha
 
       print("raw_alpha: ", raw_alpha, "filt_alpha: ", filt_alpha, "inertia_ff: ", inertia_ff, "rate_rad: ", rate_rad, "ff: ", ff, "CS.steeringAngleDeg: ", CS.steeringAngleDeg)
 
