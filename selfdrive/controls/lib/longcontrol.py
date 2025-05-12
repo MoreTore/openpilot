431efcfbb9b104ac510f157e05847cdb94aa1ef6
MoreTore <ryleymccarroll22@gmail.com>
2025-05-11 23:23:26 -0500

    fix autoresume

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -12,10 +12,13 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 def long_control_state_trans(CP, active, long_control_state, v_ego,
                              should_stop, brake_pressed, cruise_standstill):
-  stopping_condition = should_stop
+  start_condition = (not should_stop and
+                            cruise_standstill and
+                            not brake_pressed)
   starting_condition = (not should_stop and
                         not cruise_standstill and
                         not brake_pressed)
+
   started_condition = v_ego > CP.vEgoStarting
 
   if not active:
@@ -32,13 +35,13 @@ def long_control_state_trans(CP, active, long_control_state, v_ego,
           long_control_state = LongCtrlState.pid
 
     elif long_control_state == LongCtrlState.stopping:
-      if starting_condition and CP.startingState:
+      if starting_condition and CP.startingState or start_condition:
         long_control_state = LongCtrlState.starting
       elif starting_condition:
         long_control_state = LongCtrlState.pid
 
     elif long_control_state in [LongCtrlState.starting, LongCtrlState.pid]:
-      if stopping_condition:
+      if should_stop:
         long_control_state = LongCtrlState.stopping
       elif started_condition:
         long_control_state = LongCtrlState.pid
