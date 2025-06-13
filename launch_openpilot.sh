cb1b2d94763620f7eee89c6e05b170b3f2b07889
MoreTorque <104254025+MoreTore@users.noreply.github.com>
2025-06-13 17:01:37 -0500

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,4 +2,7 @@
 export API_HOST=https://api.konik.ai
 export ATHENA_HOST=wss://athena.konik.ai
 export MAPS_HOST=https://api.konik.ai/maps
+mkdir /cache
+mkdir /cache/params
+mkdir /cache/params/d
 exec ./launch_chffrplus.sh
