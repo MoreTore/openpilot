OP-Community/CommunaPilot

use konik server

common/api.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, UTC
 from openpilot.system.hardware.hw import Paths
 from openpilot.system.version import get_version
 
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'https://api.konik.com')
 
 class Api:
   def __init__(self, dongle_id):

selfdrive/ui/qt/api.h
@@ -9,7 +9,7 @@
 
 namespace CommaApi {
 
-const QString BASE_URL = util::getenv("API_HOST", "https://api.commadotai.com").c_str();
+const QString BASE_URL = util::getenv("API_HOST", "https://api.konik.com").c_str();
 QByteArray rsa_sign(const QByteArray &data);
 QString create_jwt(const QJsonObject &payloads = {}, int expiry = 3600);
 

tools/lib/api.py
@@ -1,6 +1,6 @@
 import os
 import requests
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'https://api.konik.com')
 
 class CommaApi:
   def __init__(self, token=None):

tools/replay/api.h
@@ -8,7 +8,7 @@
 
 namespace CommaApi2 {
 
-const std::string BASE_URL = util::getenv("API_HOST", "https://api.commadotai.com").c_str();
+const std::string BASE_URL = util::getenv("API_HOST", "https://api.konik.com").c_str();
 std::string create_token(bool use_jwt, const json11::Json& payloads = {}, int expiry = 3600);
 std::string httpGet(const std::string &url, long *response_code = nullptr);
 

tools/scripts/fetch_image_from_route.py
@@ -24,7 +24,7 @@ segment = int(sys.argv[2])
 frame = int(sys.argv[3])
 camera = cameras[sys.argv[4]] if len(sys.argv) > 4 and sys.argv[4] in cameras else "cameras"
 
-url = f'https://api.commadotai.com/v1/route/{route}/files'
+url = f'https://api.konik.com/v1/route/{route}/files'
 r = requests.get(url, headers={"Authorization": f"JWT {jwt}"}, timeout=10)
 assert r.status_code == 200
 print("got api response")

MoreTore/openpilot

common/api.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, UTC
 from openpilot.system.hardware.hw import Paths
 from openpilot.system.version import get_version
 
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
 
 class Api:
   def __init__(self, dongle_id):

selfdrive/ui/qt/api.h
@@ -9,7 +9,7 @@
 
 namespace CommaApi {
 
-const QString BASE_URL = util::getenv("API_HOST", "https://api.commadotai.com").c_str();
+const QString BASE_URL = util::getenv("API_HOST", "https://api.konik.ai").c_str();
 QByteArray rsa_sign(const QByteArray &data);
 QString create_jwt(const QJsonObject &payloads = {}, int expiry = 3600);
 

tools/lib/api.py
@@ -1,6 +1,6 @@
 import os
 import requests
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
 
 class CommaApi:
   def __init__(self, token=None):

tools/replay/api.h
@@ -8,7 +8,7 @@
 
 namespace CommaApi2 {
 
-const std::string BASE_URL = util::getenv("API_HOST", "https://api.commadotai.com").c_str();
+const std::string BASE_URL = util::getenv("API_HOST", "https://api.konik.ai").c_str();
 std::string create_token(bool use_jwt, const json11::Json& payloads = {}, int expiry = 3600);
 std::string httpGet(const std::string &url, long *response_code = nullptr);
 

tools/scripts/fetch_image_from_route.py
@@ -24,7 +24,7 @@ segment = int(sys.argv[2])
 frame = int(sys.argv[3])
 camera = cameras[sys.argv[4]] if len(sys.argv) > 4 and sys.argv[4] in cameras else "cameras"
 
-url = f'https://api.commadotai.com/v1/route/{route}/files'
+url = f'https://api.konik.ai/v1/route/{route}/files'
 r = requests.get(url, headers={"Authorization": f"JWT {jwt}"}, timeout=10)
 assert r.status_code == 200
 print("got api response")