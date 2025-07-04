22350cd81cd67142cb34d99aae96a8b8f327b784
MoreTore <ryleymccarroll22@gmail.com>
2025-07-03 23:04:37 -0500

    fix gen 1 standstill

--- selfdrive/car/mazda/carstate.py
+++ selfdrive/car/mazda/carstate.py
@@ -124,7 +124,7 @@ class CarState(CarStateBase):
 
     # TODO: the signal used for available seems to be the adaptive cruise signal, instead of the main on
     #       it should be used for carState.cruiseState.nonAdaptive instead
-    ret.cruiseState.standstill = cp.vl["PEDALS"]["STANDSTILL"] == 1
+    ret.cruiseState.standstill = ret.standstill
     ret.cruiseState.speed = cp.vl["CRZ_EVENTS"]["CRZ_SPEED"] * CV.KPH_TO_MS
     if self.CP.flags & MazdaFlags.RADAR_INTERCEPTOR:
       self.crz_info = copy.copy(cp_cam.vl["CRZ_INFO"])
