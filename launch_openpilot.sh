40c12df49057f1aebe300906b6095c9dfdf328c0
MoreTorque <104254025+MoreTore@users.noreply.github.com>
2025-07-13 23:11:49 -0600

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -5,8 +5,8 @@
 # export FINGERPRINT="MAZDA CX-30"
 # export FINGERPRINT="MAZDA CX-50"
 # export FINGERPRINT="MAZDA CX-90"
-export ATHENA_HOST="wss://connect-ws.duckdns.org"
-export API_HOST="https://connect-api.duckdns.org"
+export ATHENA_HOST="wss://athena.konik.ai"
+export API_HOST="https://api.konik.ai"
 
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
