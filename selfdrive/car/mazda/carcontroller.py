6ee4eed427fa1c027430152ca0885b219e56315f
Hana MacInnis <im@hana.lol>
2025-12-03 12:46:46 -0600

    fix format

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -169,3 +169,4 @@ class CarController(CarControllerBase):
     self.frame += 1
     Timer.tick()
     return new_actuators, can_sends
+  
--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -152,3 +152,4 @@ class CarInterface(CarInterfaceBase):
     ret.events = events.to_msg()
 
     return ret, fp_ret
+  
