64f822857222119ca60d79a890120556f524ac50
zyx124 <zhaoyuxin124@gmail.com>
2026-08-08 22:07:08 -0700

    Force fingerprint for 2022 Mazda CX-30

--- launch_env.sh
+++ launch_env.sh
@@ -6,6 +6,9 @@ export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
+# Force this fork to use the 2019-22 Mazda CX-30 platform.
+export FINGERPRINT="MAZDA_CX_30"
+
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="10.1"
 fi
